9f9a056b9a9eedce7f31cc1567422eb06fa47142
zinirun <heojj97@naver.com>
2020-12-02 19:16:07 +0900

    Add MyForm

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import Greetings from './Greetings'
-import Counter from './Counter'
+import MyForm from './MyForm'
 
 function App() {
-  const onClick = (name: string) => {
-    console.log(name);
+  const onSubmit = (form: {name: string; description: string}) => {
+    console.log(form)
   }
   return (
     <>
-    <Greetings name="리액트" onClick={onClick} />
-    <Counter />
+    <MyForm onSubmit={onSubmit}/>
     </>
   );
 }
